81b5258b01012e93d19ac95f96523462aacf56fe
christyl23 <120269677+christyl23@users.noreply.github.com>
2024-04-04 19:47:44 -0400

    Update Main.tf

--- Jenkins-terraform/Main.tf
+++ Jenkins-terraform/Main.tf
@@ -60,7 +60,7 @@ resource "aws_security_group" "Jenkins-sg" {
 
 resource "aws_instance" "web" {
   ami                    = "ami-08116b9957a259459"
-  instance_type          = "t2.medium"
+  instance_type          = "t2.large"
   key_name               = "my-key"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
   user_data              = templatefile("./install_jenkins.sh", {})
