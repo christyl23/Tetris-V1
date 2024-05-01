0dbc94ddaf6060a5a6bc6555336324aacea46784
christyl23 <120269677+christyl23@users.noreply.github.com>
2024-05-01 16:39:08 -0400

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "ce2e-s3" # Replace with your actual S3 bucket name
-    key    = "Jenkins/terraform.tfstate"
+    key    = "Eks/terraform.tfstate"
     region = "us-west-2"
   }
 }
